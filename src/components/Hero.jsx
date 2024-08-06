758aff58c090a71ca715ff4f5fdc790172e7b330
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-08-06 21:06:35 +0530

    banner

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { useEffect, useState } from 'react';
+import { heroVideo, smallHeroVideo } from "../utils";
 
 const Hero = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [VideoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo);
 
-export default Hero
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
+
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
+
+  useGSAP(() => {
+    gsap.to("#hero", {
+      opacity: 1,
+      delay: 1.2,
+    });
+  }, []);
+
+  return (
+    <section className='w-full nav-height bg-black relative'>
+      <div className='h-5/6 w-full flex-center flex-col '>
+        <p id='hero' className='hero-title'>iPhone 15 Pro</p>
+        <div className='md:w-10/12 w-9/12'>
+          <video className='pointer-events-none' autoPlay muted playsInline={true} key={VideoSrc}>
+            <source src={VideoSrc} type='video/mp4' />
+          </video>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
