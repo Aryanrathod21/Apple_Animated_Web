0ac2de0fc70880ecced09d3e56f50cdcd95a0d04
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-08-07 15:24:15 +0530

    useEffect(js)

--- src/components/VideoCarousel.tsx
+++ src/components/VideoCarousel.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { hightlightsSlides } from "../constants";
 
 const VideoCarousel = () => {
+    const videoRef = useRef<(HTMLVideoElement | null)[]>([]);
+    const videoSpanRef = useRef<(HTMLSpanElement | null)[]>([]);
+    const videoDivRef = useRef<(HTMLDivElement | null)[]>([]);
+    
+
+    const [video, setvideo] = useState({
+        isEnd:false,
+        startPlay:false,
+        videoId:0,
+        isLastVideo:false,
+        isPlaying:false,
+    })
+    const [loadedData, setloadedData] = useState([]);
+    const{isEnd , isPlaying , isLastVideo , videoId , startPlay } = video;
+
+    useEffect(() => {
+        if (loadedData.length > 3) {
+            if (!isPlaying) {
+              videoRef.current[videoId]?.pause();
+            } else {
+              startPlay && videoRef.current[videoId]?.play();
+            }
+          }
+    }, [startPlay,videoId,isPlaying,loadedData])
+
+    useEffect(() => {
+        let currentProgress = 0;
+        let span = videoSpanRef.current;
+        if (span[videoId]) {
+            let anim = gsap.to(span[videoId], {
+              onUpdate: () => {
+               
+              },
+              onComplete: () => {
+                
+              },
+            });
+          }
+        }, [videoId, startPlay]);
+
   return (
     <>
         <div className='flex items-center'>
@@ -14,13 +54,17 @@ const VideoCarousel = () => {
                             muted
                             playsInline={true}
                             preload='auto'
+                            ref={(el) => (videoRef.current[i] = el)}
+                            onPlay={() =>
+                                setvideo((preVideo) => ({ ...preVideo, isPlaying: true }))
+                              }
                             >
                                 <source src={list.video} type='video/mp4'/>
                             </video>
                         </div>
                         <div className='absolute top-12 left-[5%] z-10'>
                             {list.textLists.map((text) => (
-                                <p key={text}>
+                                <p key={text} className='md:text-2xl text-xl font-medium'>
                                     {text}
                                 </p>
                             ))}
